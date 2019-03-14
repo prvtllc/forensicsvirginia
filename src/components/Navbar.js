164f839280f84fdd88dfacb13b6d74785189dac0
Domingo Rivera <djr@infosecusa.com>
2019-03-14 01:19:38 -0400

    5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.png'
+//import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
   componentDidMount() {
@@ -36,8 +36,8 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="DefamationLawyer.Tech" style={{ width: '88px' }} />
+          <Link to="/" className="navbar-item" title="Logo"> 
+          <!--- just  <img src={logo} alt="Virginia Computer Forensics" style={{ width: '88px' }} /> --->
             </Link>
             {/* Hamburger menu */}
             <div className="navbar-burger burger" data-target="navMenu">
